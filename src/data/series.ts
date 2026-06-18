48fe893a6487d110e8bf3b76987c234516e79eea
Martynas <martynas.simniskis@gmail.com>
2026-06-19 00:21:11 +0300

    Initial commit

---
+++ src/data/series.ts
@@ -0,0 +1,20 @@
+export const series = [
+  {
+    title: 'Portretai',
+    slug: 'portretai',
+    cover: 'photo_10',
+    photos: ['photo_10', 'photo_6', 'photo_7', 'photo_0'],
+  },
+  {
+    title: 'Gamta',
+    slug: 'gamta',
+    cover: 'photo_11',
+    photos: ['photo_11', 'photo_4', 'photo_9', 'photo_2'],
+  },
+  {
+    title: 'Kelionės',
+    slug: 'kelliones',
+    cover: 'photo_12',
+    photos: ['photo_12', 'photo_1', 'photo_8', 'photo_5'],
+  },
+];
