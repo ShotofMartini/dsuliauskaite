14709f96cdef88760af7a788932cdd0fab06e38e
Martynas <martynas.simniskis@gmail.com>
2026-06-19 00:29:37 +0300

    Add GitHub Actions deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://shotofmartini.github.io',
+  base: '/dsuliauskaite',
   vite: {
     plugins: [tailwindcss()]
   }
